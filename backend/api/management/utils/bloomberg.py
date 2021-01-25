1e579392983aa70d9b4e24e50ec9343b415bc0b9
fyk7 <tfscofield115@gmail.com>
2021-01-25 11:23:42 +0900

    add retry func for bloomberg

--- backend/api/management/utils/bloomberg.py
+++ backend/api/management/utils/bloomberg.py
@@ -17,14 +17,23 @@ def prep_text(text):
     return text
 
 
+def retry(func, retry_count, wait_time, **kwargs):
+    if retry_count == 0:
+        return None
+    try:
+        return func(**kwargs)
+    except Exception as e:
+        logger.error(
+            f'Error failed to {str(func)}. retry_count = {retry_count}, error: {str(e)}')
+        time.sleep(wait_time)
+    return retry(func, retry_count - 1, wait_time, **kwargs)
+
+
 # TODO Add exception handling when css selectors no longer work.
 def fetch_news():
     target_url = BLOOMBERG_URL
-    try:
-        res = requests.get(target_url)
-        res.raise_for_status()
-    except Exception as e:
-        logger.error(e, stack_info=True, exc_info=True)
+    res = retry(requests.get, retry_count=3,
+                wait_time=1, url=target_url)
     time.sleep(TIME_SLEEP)
 
     soup = BeautifulSoup(res.text, 'html.parser')
@@ -38,16 +47,13 @@ def fetch_news():
         news_dict['title'] = a.text
         child_url = target_url + a["href"]
         news_dict['detail_url'] = child_url
-        try:
-            res = requests.get(child_url)
-            res.raise_for_status()
-        except Exception as e:
-            logger.error(e, stack_info=True, exc_info=True)
+        res = retry(requests.get, retry_count=3,
+                    wait_time=1, url=child_url)
         time.sleep(TIME_SLEEP)
         soup = BeautifulSoup(res.text, 'html.parser')
         news_dict['detail'] = ''.join([prep_text(c.text) for c in soup.select(
             "article div.content-well section div.body-columns div p")])[:980]
         news_dict['article_timestamp'] = soup.select(
-            "body > main > div.transporter-item.current > article > div.lede-text-only.lede > div > div > div > time")[0]["datetime"]
+            "body main div.transporter-item.current article div div div div time")[0]["datetime"]
         news_list.append(news_dict)
     return news_list
