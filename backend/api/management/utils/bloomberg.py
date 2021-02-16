2d3f22a59888466c3419cbd557fa8cca7758cd6c
fyk7 <tfscofield115@gmail.com>
2021-02-16 15:51:16 +0900

    change some funcs to local funcs

--- backend/api/management/utils/bloomberg.py
+++ backend/api/management/utils/bloomberg.py
@@ -9,7 +9,7 @@ TIME_SLEEP = 0.5
 logger = getLogger('django')
 
 
-def prep_text(text):
+def _prep_text(text):
     text = re.sub(r'原題.*(抜粋)', '', text)
     text = re.sub(r'Photographer:.*', '', text)
     text = re.sub(r'Source:.*', '', text)
@@ -17,7 +17,7 @@ def prep_text(text):
     return text
 
 
-def retry(func, retry_count, wait_time, **kwargs):
+def _retry(func, retry_count, wait_time, **kwargs):
     if retry_count == 0:
         return None
     try:
@@ -26,13 +26,13 @@ def retry(func, retry_count, wait_time, **kwargs):
         logger.error(
             f'Error failed to {str(func)}. retry_count = {retry_count}, error: {str(e)}')
         time.sleep(wait_time)
-    return retry(func, retry_count - 1, wait_time, **kwargs)
+    return _retry(func, retry_count - 1, wait_time, **kwargs)
 
 
 # TODO Add exception handling when css selectors no longer work.
 def fetch_news():
     target_url = BLOOMBERG_URL
-    res = retry(requests.get, retry_count=3,
+    res = _retry(requests.get, retry_count=3,
                 wait_time=1, url=target_url)
     time.sleep(TIME_SLEEP)
 
@@ -47,11 +47,11 @@ def fetch_news():
         news_dict['title'] = a.text
         child_url = target_url + a["href"]
         news_dict['detail_url'] = child_url
-        res = retry(requests.get, retry_count=3,
+        res = _retry(requests.get, retry_count=3,
                     wait_time=1, url=child_url)
         time.sleep(TIME_SLEEP)
         soup = BeautifulSoup(res.text, 'html.parser')
-        news_dict['detail'] = ''.join([prep_text(c.text) for c in soup.select(
+        news_dict['detail'] = ''.join([_prep_text(c.text) for c in soup.select(
             "article div.content-well section div.body-columns div p")])[:980]
         news_dict['article_timestamp'] = soup.select(
             "body main div.transporter-item.current article div div div div time")[0]["datetime"]
